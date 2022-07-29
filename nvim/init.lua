3d48b7db9a9b9fa1a1bf0e4e0b7687e90a120df9
BitlyTwiser <joshua.groeschl@tutanota.com>
2022-07-29 16:41:43 -0700

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,5 +15,8 @@ vim.cmd([[
 ]])                
 
 vim.api.nvim_set_keymap("n", "<leader>bk", ":Vex<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader><CR>", "so ~/.config/nvim/init.lua<CR>", { noremap = true })
-
+vim.api.nvim_set_keymap("n", "<leader><CR>", ":luafile ~/.config/nvim/init.lua<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", { noremap = true })
