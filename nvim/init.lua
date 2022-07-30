da817902c2803c86f020c035fe60601d8165ffad
BitlyTwiser <joshua.groeschl@tutanota.com>
2022-07-29 18:07:22 -0700

    more

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,7 @@ vim.cmd([[
  set tabstop=4 softtabstop=4
  set shiftwidth=4           
  set smartindent 
- colorscheme desert
+ colorscheme terafox
  au TextYankPost * silent! lua vim.highlight.on_yank()
  let mapleader = " "                                  
 ]])                
@@ -18,5 +18,10 @@ vim.api.nvim_set_keymap("n", "<leader>bk", ":Vex<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader><CR>", ":luafile ~/.config/nvim/init.lua<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fbuff", "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope file_browser<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tr", ":NvimTreeToggle<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tf", ":NvimTreeFindFile<CR>", { noremap = true })
+
+require("nvim-tree").setup()
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,5 @@
+local use = require('packer').use
+
 return require("packer").startup(function()
 use {
   'nvim-telescope/telescope.nvim', tag = '0.1.0',
@@ -7,4 +9,15 @@ use {
  'nvim-treesitter/nvim-treesitter',
  run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
 }
+use { "nvim-telescope/telescope-file-browser.nvim" }
+use "EdenEast/nightfox.nvim"
+use {
+  'kyazdani42/nvim-tree.lua',
+  requires = {
+    'kyazdani42/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
 end)
