15875760c665ee1c0fd180b157280478e32cddb6
BitlyTwiser <joshua.groeschl@tutanota.com>
2022-07-29 18:53:08 -0700

    Better

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,6 @@
-require('plugins')
+require('bitlytwiser.plugins')
+require('bitlytwiser.nvim-tree')
+
 
 vim.cmd([[
  set guicursor=
@@ -23,5 +25,5 @@ vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin'
 vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope file_browser<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>tr", ":NvimTreeToggle<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>tf", ":NvimTreeFindFile<CR>", { noremap = true })
-
-require("nvim-tree").setup()
+vim.api.nvim_set_keymap("n", "<leader>ct", ":NvimTreeClose<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>rt", ":NvimTreeRefresh<CR>", { noremap = true })
