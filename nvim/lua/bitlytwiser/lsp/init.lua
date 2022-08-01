3cc9a6682725b46be6923e43125cc28781a66756
BitlyTwiser <joshua.groeschl@tutanota.com>
2022-07-31 20:52:24 -0700

    Language Servers

--- nvim/lua/bitlytwiser/lsp/init.lua
+++ nvim/lua/bitlytwiser/lsp/init.lua
@@ -1,1 +1,6 @@
 require('bitlytwiser.lsp.vue')
+require('bitlytwiser.lsp.json')
+require('bitlytwiser.lsp.go')
+require('bitlytwiser.lsp.typescript')
+require('bitlytwiser.lsp.lua_lsp')
+require('bitlytwiser.lsp.ruby')
