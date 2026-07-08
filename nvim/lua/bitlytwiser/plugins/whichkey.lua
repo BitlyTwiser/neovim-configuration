5d983a0bc78f9f45e0a223d3864224cb9ec135cb
BitlyTwiser <joshua.groeschl@tutanota.com>
2026-07-08 13:00:30 -0700

    Add which-key popup for discoverable keybindings
    Add folke/which-key.nvim: pressing the leader key pops up a menu of the
    available mappings with their descriptions, plus <leader>? to list the
    current buffer's keymaps. Group labels for the leader prefixes; individual
    entries come from the desc fields already on each mapping.

---
+++ nvim/lua/bitlytwiser/plugins/whichkey.lua
@@ -0,0 +1,26 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "modern",
+    -- Labels for key prefixes (the individual mappings come from the `desc`
+    -- fields on each keymap automatically).
+    spec = {
+      { "<leader>f", group = "Find (Telescope)" },
+      { "<leader>t", group = "File tree" },
+      { "<leader>x", group = "Diagnostics (Trouble)" },
+      { "<leader>d", group = "Debug" },
+      { "<leader>r", group = "Rename / refresh / run" },
+      { "g", group = "Goto" },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Show buffer keymaps (which-key)",
+    },
+  },
+}
